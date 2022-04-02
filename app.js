679d087365996d783fbf7ecfbda3012a4843d76e
DevWesly <dev.lucaswesley@gmail.com>
2022-04-02 14:15:28 -0300

    Aula do dia 30

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 // Importar o express
 const express = require('express');
+
 //importar os reteadores
 const UsuariosRouter = require('./routes/UsuariosRouter');
 const ContatosRouter = require("./routes/ContatosRouter");
+
 // Criar um servidor/aplicação com o express
 const app = express(); 
+
 //configurar o ejs como template engine
 app.set('view engine','ejs'); 
+//configurando a pasta public para arquivos estaticos
+app.use(express.static("public"));
+
 
 // Criar rota get no endereço '/' para responder requisição com msg "olá"
 app.get('/',(req,res)=>{res.send('olá');});
+
 // usando os roteadores
 app.use("/", UsuariosRouter);
 app.use("/",ContatosRouter);
